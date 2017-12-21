eb176d5d917c19952dd5220c6bce33eb6e67106d
Elli Scharlin <escharlin@wesleyan.edu>
2017-12-21 17:36:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,11 @@
-row0 = " #{board[0]} | #{board[1]} | #{board[2]} "
-lines = "-----------"
-row1 = " #{board[3]} | #{board[4]} | #{board[5]} "
-row2 = " #{board[6]} | #{board[7]} | #{board[8]} "
-board = [row0, lines, row1, lines, row2]
+
 def display_board(board)
-  puts board
+  row0 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  lines = "-----------"
+  row1 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row2 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  playing_board = [row0, lines, row1, lines, row2]
+  puts playing_board
 end
 
 def input_to_index(input)
