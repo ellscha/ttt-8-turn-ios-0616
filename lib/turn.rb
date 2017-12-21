a8863c671b5e344325a037c9fb5e7b7a444196a1
Elli Scharlin <escharlin@wesleyan.edu>
2017-12-21 17:43:55 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,7 @@ def move(index, board, value="X")
   end
 end
 #turn will ask the user for an input in the bin file
-def turn(input, board, value="X")
+def turn(board)
   input_to_index(input)
   if valid_move?(input)
     move(input, board, value)
