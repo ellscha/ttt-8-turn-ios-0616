302db15c59879608721b7c09d42c3da6d784b9df
Elli Scharlin <escharlin@wesleyan.edu>
2017-12-21 18:09:28 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(board)
   input = input_to_index(index)
   if valid_move?(board, input)
     move(board, input)
-    display_board
+    display_board(board)
   else
     turn(board)
   end
